104e851fdb4ff6b39bd37f2877704361c739f4bc
Young Min Joo <ymjoo827@gmail.com>
2022-04-21 23:51:15 +0900

    completed up to #11

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,34 @@
+""" Function-Based View
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from . import models
 
-# Create your views here.
+def all_rooms(request):
+
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.page(int(page))
+    return render(request, "rooms/home.html", {"page": rooms})
+"""
+
+from django.views.generic import ListView
+from django.shortcuts import render
+from . import models
+
+
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    print(pk)
+    return render(request, "rooms/detail.html")
